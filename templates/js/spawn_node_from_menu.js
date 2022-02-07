1e0ba22d2eeb4e68c2b9c4499d03da82fa0ae1cc
zahrada <volovitc@gmail.com>
2022-02-08 00:55:19 +0300

    pizda

--- templates/js/spawn_node_from_menu.js
+++ templates/js/spawn_node_from_menu.js
@@ -1,19 +1,31 @@
 
-import {instantiateHTMLNode} from './node_instantiate.js'
-
-const create_node_tab = document.getElementById('create_node')
-const create_BasicNode_tab = document.getElementById('create_BasicNode')
-//nodes_pool = new Array(20)
 
 
+import {NodeInstantiatorClass} from './node_instantiate.js'
 
-create_BasicNode_tab.addEventListener('click', event => {
-    console.log(event.pageX, event.pageY)    
-    //nodes_pool.push(new BasicNode(event.clientX, event.clientY))
-    contextMenu.classList.remove("visible")
-    instantiateHTMLNode(event.pageX, event.pageY,new Date().valueOf())
+class qwe{
+    constructor(){
+    this.create_node_tab = document.getElementById('create_node')
+    this.create_BasicNode_tab = document.getElementById('create_BasicNode')
+    this.nodes_pool = new Array(20)
+    asd()
+    }
 
-})
+    asd(){
+        this.create_BasicNode_tab.addEventListener('click', event => {
+        console.log(event.pageX, event.pageY)    
+        //nodes_pool.push(new BasicNode(event.clientX, event.clientY))
+        contextMenu.classList.remove("visible")        
+        let a  = new NodeInstantiatorClass(event.pageX, event.pageY,new Date().valueOf())
+        nodes_pool.push(a)
+
+    }}
+    
+
+}
+
+
+
 
 
 
