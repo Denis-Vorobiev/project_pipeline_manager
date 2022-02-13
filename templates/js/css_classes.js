be1bf241744cb3ecd6d62ee105f491bbb89c5885
zahrada <volovitc@gmail.com>
2022-02-13 22:55:04 +0300

    я в ахуе

---
+++ templates/js/css_classes.js
@@ -0,0 +1,27 @@
+export class CSSVarsClass {
+  constructor() {
+    
+  }
+}
+
+export let basicStyle = new CSSVarsClass()
+basicStyle.hidden = "display:none;"
+basicStyle.visible = "display:block"
+basicStyle.baseCard = 'base_card'
+basicStyle.cardHeader = "card_header d-flex justify-content-between"
+basicStyle.shortDescription = "short_desc mb-auto"
+basicStyle.textArea = "form-control form-control-sm"
+basicStyle.executorDiv = "executor d-flex justify-content-between"
+basicStyle.exeNameDivInput = "form-control form-control-sm"
+basicStyle.allignMid = "align-middle"
+basicStyle.exeSlotDivInput ="form-control form-control-sm right_side_align"
+basicStyle.resourcesDiv ="mt-auto  recource_sq" 
+basicStyle.subResoDiv = "d-flex  justify-content-between h-auto"
+basicStyle.timeDivKey = "key h-auto align-middle"
+basicStyle.resoDivValue = "value h-auto right_side_align "
+basicStyle.resoDivValueInput = "form-control right_side_align" 
+basicStyle.moneyDivKey =  "key h-auto align-middle"
+basicStyle.submitBtn = "btn btn-primary input_submit_btn"
+basicStyle.editBtn = "btn btn-outline-light edit_btn"
+basicStyle.removeBtn = "remove_btn"
+
